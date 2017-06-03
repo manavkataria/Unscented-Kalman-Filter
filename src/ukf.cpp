81217e28a4d37bd816c443a44ccaaeef47cdfad2
Manav Kataria <manavkataria.iitb@gmail.com>
2017-06-03 11:10:07 -0700

    Tweak P_ and std_* values for optimal prediction.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -53,32 +53,43 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(n_x_, n_x_);
   P_ <<
-      100, 0, 0, 0, 0,
-      0, 100, 0, 0, 0,
-      0, 0, 100, 0, 0,
-      0, 0, 0, 100, 0,
-      0, 0, 0, 0, 100;
+      1, 0, 0,  0, 0,
+      0, 1, 0,  0, 0,
+      0, 0, 50, 0, 0,
+      0, 0, 0, 50, 0,
+      0, 0, 0, 0, 50;
+
+  // Desired RMSE: [.09, .10, .40, .30]
+
+/* Current RMSE:
+RMSE
+0.0830734
+0.095111
+0.365387
+0.296387
+Done!
+*/
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.8;
+  std_a_ = 0.1;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.6;
+  std_yawdd_ = 0.1;
 
   // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
+  std_laspx_ = 0.015;
 
   // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
+  std_laspy_ = 0.015;
 
   // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
+  std_radr_ =  0.6;
 
   // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
+  std_radphi_ = 0.01;
 
   // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
+  std_radrd_ = 0.1;
 
   // Initialize predicted sigma points matrix
   Xsig_pred_ = MatrixXd(n_x_, 2*n_aug_+1);
